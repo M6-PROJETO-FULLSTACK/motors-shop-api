ff9580083ffc1811e0a690cec38fd5f614c53684
anavgbc <agomesbc@gmail.com>
2023-02-24 13:04:31 -0300

    fix: saving gallery in db

--- src/routes/vehicles.routes.ts
+++ src/routes/vehicles.routes.ts
@@ -4,11 +4,9 @@ import handleErrorMiddleware from "../middlewares/handleError.middleware";
 import listVehiclesController from "../controllers/vehicles/listVehicles.controller";
 import listOneVehicleController from "../controllers/vehicles/listOneVehicle.controller";
 import ensureAuthMiddleware from "../middlewares/ensureAuth.middleware";
-import {
-  createVehiclesControllers,
-  deleteVehicleController,
-} from "../controllers/vehicles.controllers";
 import updateVehiclesController from "../controllers/vehicles/updateVehicle.controller";
+import { createVehiclesControllers } from "../controllers/vehicles/createVehicle.controller";
+import { deleteVehicleController } from "../controllers/vehicles/deleteVehicle.controller";
 
 const vehiclesRoutes = Router();
 
@@ -17,6 +15,6 @@ vehiclesRoutes.get("/:id", handleErrorMiddleware, listOneVehicleController);
 
 vehiclesRoutes.post("", ensureAuthMiddleware, createVehiclesControllers);
 vehiclesRoutes.delete("/:id", ensureAuthMiddleware, deleteVehicleController);
-vehiclesRoutes.patch("/:id", ensureAuthMiddleware, updateVehiclesController)
+vehiclesRoutes.patch("/:id", ensureAuthMiddleware, updateVehiclesController);
 
 export default vehiclesRoutes;
