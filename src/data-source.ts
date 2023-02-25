e7c1e712a047922f26f22ce4b765e1b531d476cd
Gabriel Martins <lima.gabriel@sou.ufac.br>
2023-02-25 01:49:23 -0500

    feat: fixes migrations

--- src/data-source.ts
+++ src/data-source.ts
@@ -8,31 +8,33 @@ import { Vehicle } from "./entities/vehicles.entities";
 import { initialMigration1677081482026 } from "./migrations/1677081482026-initialMigration";
 import { createTables1677081832907 } from "./migrations/1677081832907-createTables";
 import { updateGalleries1677101810260 } from "./migrations/1677101810260-updateGalleries";
+import { hotfixes1677307072303 } from "./migrations/1677307072303-hotfixes";
 
 const AppDataSource = new DataSource(
-  process.env.NODE_ENV === "test"
-    ? {
-        type: "sqlite",
-        database: ":memory:",
-        synchronize: true,
-        entities: ["src/entities/*.ts"],
-      }
-    : {
-        type: "postgres",
-        host: process.env.HOST,
-        port: 5432,
-        username: process.env.POSTGRES_USER,
-        password: process.env.POSTGRES_PASSWORD,
-        database: process.env.POSTGRES_DB,
-        logging: true,
-        synchronize: false,
-        entities: [Adress, Comment, Gallery, User, Vehicle],
-        migrations: [
-          initialMigration1677081482026,
-          createTables1677081832907,
-          updateGalleries1677101810260,
-        ],
-      }
+	process.env.NODE_ENV === "test"
+		? {
+				type: "sqlite",
+				database: ":memory:",
+				synchronize: true,
+				entities: [Adress, Comment, Gallery, User, Vehicle],
+		  }
+		: {
+				type: "postgres",
+				host: process.env.HOST,
+				port: 5432,
+				username: process.env.POSTGRES_USER,
+				password: process.env.POSTGRES_PASSWORD,
+				database: process.env.POSTGRES_DB,
+				logging: true,
+				synchronize: false,
+				entities: [Adress, Comment, Gallery, User, Vehicle],
+				migrations: [
+					initialMigration1677081482026,
+					createTables1677081832907,
+					updateGalleries1677101810260,
+					hotfixes1677307072303,
+				],
+		  }
 );
 
 export default AppDataSource;
