7271841389e74a231ed768baa1df16dd2302cc38
anavgbc <agomesbc@gmail.com>
2023-02-28 15:40:26 -0300

    feat: calling comments routes

--- src/app.ts
+++ src/app.ts
@@ -4,12 +4,14 @@ import express from "express";
 import vehiclesRoutes from "./routes/vehicles.routes";
 import usersRoutes from "./routes/users.routes";
 import handleErrorMiddleware from "./middlewares/handleError.middleware";
+import commentsRoutes from "./routes/comments.routes";
 
 const app = express();
 app.use(express.json());
 
 app.use("/vehicles", vehiclesRoutes);
 app.use("/users", usersRoutes);
+app.use("/comments", commentsRoutes);
 
 app.use(handleErrorMiddleware);
 
