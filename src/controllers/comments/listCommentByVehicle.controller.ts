03b267e4585e158ea99905abe0bd9a1a58b59952
anavgbc <agomesbc@gmail.com>
2023-02-28 15:38:53 -0300

    feat: listing comments by vehicle

---
+++ src/controllers/comments/listCommentByVehicle.controller.ts
@@ -0,0 +1,12 @@
+import { Request, Response } from "express";
+import listCommentsByVehicleService from "../../services/comments/listCommentsByVehicle.service";
+
+const listCommentByVehicleController = async (req: Request, res: Response) => {
+  const { id } = req.params;
+
+  const comments = await listCommentsByVehicleService(id);
+
+  return res.status(201).json(comments);
+};
+
+export default listCommentByVehicleController;
