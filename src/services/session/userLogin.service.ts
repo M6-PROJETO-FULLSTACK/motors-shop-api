3c167b803a6d8cba0a1d90c88990519c4745b304
anavgbc <agomesbc@gmail.com>
2023-03-02 18:22:44 -0300

    fix: token user_id

--- src/services/session/userLogin.service.ts
+++ src/services/session/userLogin.service.ts
@@ -1,30 +1,30 @@
 import AppDataSource from "../../data-source";
 import { User } from "../../entities/users.entities";
 import { IUserLogin } from "../../interfaces/User";
-import bcrypt from "bcrypt"
-import jwt from 'jsonwebtoken'
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 import AppError from "../../errors/appError";
 
-const userLoginService = async ({email, password}:IUserLogin) => {
-    const userRepository = AppDataSource.getRepository(User)
-    const users = await userRepository.find()
+const userLoginService = async ({ email, password }: IUserLogin) => {
+  const userRepository = AppDataSource.getRepository(User);
+  const users = await userRepository.find();
 
-    const user = users.find(user => user.email === email)
+  const user = users.find((user) => user.email === email);
 
-    if(!user){
-        throw new AppError('Wrong email or password', 401)
-    }
-    if(!bcrypt.compareSync(password, user.password)){
-        throw new AppError('Wrong email or password', 401)
-    }
+  if (!user) {
+    throw new AppError("Wrong email or password", 401);
+  }
+  if (!bcrypt.compareSync(password, user.password)) {
+    throw new AppError("Wrong email or password", 401);
+  }
 
-    const token = jwt.sign(
-        {email:email},
-        String(process.env.SECRET_KEY),
-        {expiresIn: '12h'}
-    )
+  const token = jwt.sign(
+    { email: email, id: user.id },
+    String(process.env.SECRET_KEY),
+    { expiresIn: "12h" }
+  );
+  console.log(token);
+  return { user, token };
+};
 
-    return {user,token}
-}
-
-export default userLoginService
+export default userLoginService;
