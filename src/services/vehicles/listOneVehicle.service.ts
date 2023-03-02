d290cb568b5f08bbb5bbb5b929723fd559feae43
anavgbc <agomesbc@gmail.com>
2023-03-02 18:24:38 -0300

    fix: returning user and comments data

--- src/services/vehicles/listOneVehicle.service.ts
+++ src/services/vehicles/listOneVehicle.service.ts
@@ -1,20 +1,32 @@
 import AppDataSource from "../../data-source";
 import { Vehicle } from "../../entities/vehicles.entities";
+import { Comment } from "../../entities/comments.entities";
 import AppError from "../../errors/appError";
 
-const listOneVehicleService = async (id:string) => {
+const listOneVehicleService = async (id: string) => {
+  const vehiclesRepository = AppDataSource.getRepository(Vehicle);
+  const commentRepository = AppDataSource.getRepository(Comment);
 
-    const vehiclesRepository = AppDataSource.getRepository(Vehicle)
+  const vehicles = await vehiclesRepository.find();
 
-    const vehicles = await vehiclesRepository.find()
+  const vehicle = vehicles.find((v) => v.id === id);
 
-    const vehicle = vehicles.find(v => v.id === id)
+  const comments = await commentRepository.find({
+    where: {
+      vehicle_id: id,
+    },
+    relations: {
+      user: true,
+    },
+  });
 
-    if(!vehicle){
-        throw new AppError ("Vehicle not found.", 404)
-    }
+  if (!vehicle) {
+    throw new AppError("Vehicle not found.", 404);
+  }
 
-    return vehicle
-}
+  vehicle.comments = comments;
 
-export default listOneVehicleService
+  return vehicle;
+};
+
+export default listOneVehicleService;
