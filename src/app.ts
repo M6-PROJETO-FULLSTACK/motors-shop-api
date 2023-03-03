985cd02ff8172a283437eb1cd6437c99bb5097fb
Gabriel Martins <lima.gabriel@sou.ufac.br>
2023-03-03 00:34:42 -0500

    feat: add cors

--- src/app.ts
+++ src/app.ts
@@ -5,8 +5,12 @@ import vehiclesRoutes from "./routes/vehicles.routes";
 import usersRoutes from "./routes/users.routes";
 import handleErrorMiddleware from "./middlewares/handleError.middleware";
 
+
 const app = express();
 app.use(express.json());
+
+const cors = require('cors');
+app.use(cors());
 
 app.use("/vehicles", vehiclesRoutes);
 app.use("/users", usersRoutes);
