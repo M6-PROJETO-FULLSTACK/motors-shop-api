b0067c1c576aaa8110781c1f79303a02a1ad760a
unknown <brenos93@gmail.com>
2023-03-07 19:20:56 -0300

    fix: adicionando o campo userId e username no retorno do get vehicles

--- src/services/vehicles/listVehicles.service.ts
+++ src/services/vehicles/listVehicles.service.ts
@@ -1,13 +1,30 @@
 import AppDataSource from "../../data-source";
 import { Vehicle } from "../../entities/vehicles.entities";
+import { IVehicleProps } from "./listAllVehiclesToUser.service";
 
 const listVehiclesService = async () => {
 
     const vehiclesRepository = AppDataSource.getRepository(Vehicle)
 
-    const vehicles = await vehiclesRepository.find()
+    const vehicles = await vehiclesRepository.find({
+        relations: {
+            user: true
+        }
+    })
+    
+    const resData: IVehicleProps[] = []
 
-    return vehicles
+    vehicles.filter((elem: any, index: number) => {
+        elem.userName = vehicles[index].user.name
+        elem.userId = vehicles[index].user.id
+        resData.push(elem)
+    })
+
+    resData.filter((elem: any) => {
+        delete elem.user
+    })
+    
+    return resData
 
 }
 
