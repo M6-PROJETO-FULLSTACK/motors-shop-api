fcfc764819ffed9e601cde7fb58021bcb6dd6ac2
Breno S. Figueiredo <80117189+brenofigueiredoo@users.noreply.github.com>
2023-03-14 16:33:15 -0300

    Update data-source.ts

--- src/data-source.ts
+++ src/data-source.ts
@@ -22,6 +22,10 @@ const AppDataSource = new DataSource(
     : {
         type: "postgres",
         url: process.env.DATABASE_URL,
+        ssl:
+          process.env.NODE_ENV === "production"
+            ? { rejectUnauthorized: false }
+            : false,
         logging: true,
         synchronize: false,
         entities: [Adress, Comment, Gallery, User, Vehicle],
